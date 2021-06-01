edd0a871099c80d2e91fbdb81a008f866cfa906f
Hiruni Wijewardena <hiruniwijewardena@gmail.com>
2021-06-01 10:24:15 +0530

    adding changes to function LCD-String-xy

--- LCD16X2_4BIT.h
+++ LCD16X2_4BIT.h
@@ -83,6 +83,7 @@ void LCD_String_xy (char row, char pos, char *str)	/* Send string to LCD with xy
 	else if (row == 1 && pos<16)
 	LCD_Command((pos & 0x0F)|0xC0);	/* Command of first row and required position<16 */
 	LCD_String(str);		/* Call LCD string function */
+	
 }
 
 void LCD_Clear()
