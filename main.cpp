cfd9fbccfc859c35f58eb03c9717a0ca3e44039f
BhagyaYasarathna <84383159+BhagyaYasarathna@users.noreply.github.com>
2021-06-02 11:58:12 +0530

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -121,3 +121,78 @@ void LCD_Clear()
 	_delay_ms(2);
 	LCD_Command (0x80);		/* Cursor at home position */
 }
+
+
+char keyfind()
+{
+	while(1)
+	{
+		KEY_DDR = 0xF0;           /* set port direction as input-output */
+		KEY_PRT = 0xF7;
+
+		do
+		{
+			KEY_PRT &= 0x07;      /* mask PORT for column read only */
+			asm("NOP");
+			colloc = (KEY_PIN & 0x07); /* read status of column */
+		}while(colloc != 0x07);
+		
+		do
+		{
+			do
+			{
+				_delay_ms(20);             /* 20ms key debounce time */
+				colloc = (KEY_PIN & 0x07); /* read status of column */
+				}while(colloc == 0x07);        /* check for any key press */
+				
+				_delay_ms (40);	            /* 20 ms key debounce time */
+				colloc = (KEY_PIN & 0x07);
+			}while(colloc == 0x07);
+
+			/* now check for rows */
+			KEY_PRT = 0xE7;            /* check for pressed key in 1st row */
+			asm("NOP");
+			colloc = (KEY_PIN & 0x07);
+			if(colloc != 0x07)
+			{
+				rowloc = 0;
+				break;
+			}
+
+			KEY_PRT = 0xD7;		/* check for pressed key in 2nd row */
+			asm("NOP");
+			colloc = (KEY_PIN & 0x07);
+			if(colloc != 0x07)
+			{
+				rowloc = 1;
+				break;
+			}
+			
+			KEY_PRT = 0xB7;		/* check for pressed key in 3rd row */
+			asm("NOP");
+			colloc = (KEY_PIN & 0x07);
+			if(colloc != 0x07)
+			{
+				rowloc = 2;
+				break;
+			}
+
+			KEY_PRT = 0x77;		/* check for pressed key in 4th row */
+			asm("NOP");
+			colloc = (KEY_PIN & 0x07);
+			if(colloc != 0x07)
+			{
+				rowloc = 3;
+				break;
+			}
+		}
+
+		if(colloc == 0x06)
+		return(keypad[rowloc][0]);
+		else if(colloc == 0x05)
+		return(keypad[rowloc][1]);
+		else if(colloc == 0x03)
+		return(keypad[rowloc][2]);
+		else
+		return(keypad[rowloc][3]);
+	} 
