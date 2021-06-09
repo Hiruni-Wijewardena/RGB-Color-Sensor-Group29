01adc2b31d494ec49e5e5d4c1c0d936c4c016747
Hiruni Wijewardena <hiruniwijewardena@gmail.com>
2021-06-09 15:09:35 +0530

    Updated the getNum function

--- main.cpp
+++ main.cpp
@@ -39,21 +39,17 @@ int ADC_Read(char channel)
 
 double getnum()
 {
-	double num= 0;
+	long num= 0;
 	char key;
 	double ikey;
-	double mult;
-	char numberr;
+	long mult;
+	char numberr[3];
 	for (double i=0;i<=2;i++)
 	{
 		
 		key=keyfind();
 		ikey = key-'0';
-		itoa (ikey,numberr,10);
-		LCD_String_xy(0,0,numberr);
-		_delay_ms(300);
-		LCD_Clear();
-		mult = pow(10,(2-i));
+		mult = round(pow(10,(2-i)));
 		num+= mult*(ikey);
 		
 	}
